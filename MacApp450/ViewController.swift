a19948e565663687d641e1116e71c49fcc290437
tyrenwalker461 <tyrenwalker461@gmail.com>
2022-04-04 15:05:36 -0400

    Initial Commit

---
+++ MacApp450/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacApp450
+//
+//  Created by Tyren Walker on 4/4/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
